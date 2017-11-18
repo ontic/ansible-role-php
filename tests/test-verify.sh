5f29ffba6837b7dc36a35e9cde2a9b1a8588ede3
Adam Dyson <contact@adamdyson.com.au>
2017-11-19 04:13:11 +1100

    Debug a little deeper because systemctl is providing nothing

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Debug
+docker exec --tty ${container_id} env TERM=xterm /usr/sbin/php53-fpm --fpm-config /etc/php53/fpm/php-fpm.conf -t
 docker exec --tty ${container_id} env TERM=xterm systemctl status php53-fpm.service
 docker exec --tty ${container_id} env TERM=xterm journalctl -xe
 
