986ab702dc1f0b0f8eb90c662b3ad70318104a34
Adam Dyson <contact@adamdyson.com.au>
2017-11-19 01:45:17 +1100

    Verify the installed location for PHP

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Verify the installed PHP location.
+docker exec --tty ${container_id} env TERM=xterm which php
+
 # Verify the installed PHP version.
 docker exec --tty ${container_id} env TERM=xterm php --version
 
