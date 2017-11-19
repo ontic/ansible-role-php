ef21276f47a923746e240a667f57d79ac4d13a38
Adam Dyson <contact@adamdyson.com.au>
2017-11-20 02:48:42 +1100

    All of this difficulty getting PHP 5.3 to install is caused by the PPA maintainer and or the fact that the systemd service provided requires a flag within a file (which is managed manually) to be un-commented.

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [ "${playbook}" = 'test-php-5.3.yml' ]; then
-    # Debug PHP 5.3
-    docker exec --tty ${container_id} env TERM=xterm cat /etc/default/php53-fpm
-    docker exec --tty ${container_id} env TERM=xterm /usr/sbin/php53-fpm --fpm-config /etc/php53/fpm/php-fpm.conf -t
-    docker exec --tty ${container_id} env TERM=xterm systemctl status php53-fpm.service
-    docker exec --tty ${container_id} env TERM=xterm journalctl -xe
-fi
-
 # Verify the installed PHP location.
 docker exec --tty ${container_id} env TERM=xterm which php
 
