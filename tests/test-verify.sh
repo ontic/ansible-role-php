99fd5cb16c88fd9422e9411642105e50e8e0144e
Adam Dyson <contact@adamdyson.com.au>
2017-11-19 23:27:20 +1100

    Continuing to debug why FPM for PHP 5.3 is failing

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Debug
-docker exec --tty ${container_id} env TERM=xterm /etc/default/php53-fpm
-docker exec --tty ${container_id} env TERM=xterm /usr/sbin/php53-fpm --fpm-config /etc/php53/fpm/php-fpm.conf -t
-docker exec --tty ${container_id} env TERM=xterm systemctl status php53-fpm.service
-docker exec --tty ${container_id} env TERM=xterm journalctl -xe
+if [ "${playbook}" = 'test-php-5.3.yml' ]; then
+    # Debug PHP 5.3
+    docker exec --tty ${container_id} env TERM=xterm cat /etc/default/php53-fpm
+    docker exec --tty ${container_id} env TERM=xterm /usr/sbin/php53-fpm --fpm-config /etc/php53/fpm/php-fpm.conf -t
+    docker exec --tty ${container_id} env TERM=xterm systemctl status php53-fpm.service
+    docker exec --tty ${container_id} env TERM=xterm journalctl -xe
+fi
 
 # Verify the installed PHP location.
 docker exec --tty ${container_id} env TERM=xterm which php
